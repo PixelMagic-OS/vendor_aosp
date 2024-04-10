9f826cbbdee710cb9302e676d1bd395cc5e994b2
Magicxavi <rolfpeterkesche@gmail.com>
2024-04-10 21:08:42 +0300

    Revert "vendor: Add Black theme [3/3]"
    This reverts commit 272795b17e06e58fe510554b33d23ba9f12cb021.

--- config/common.mk
+++ config/common.mk
@@ -232,7 +232,6 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/aosp/overlay/common
 
 PRODUCT_PACKAGES += \
-    AndroidBlackThemeOverlay \
     CertifiedPropsOverlay \
     DocumentsUIOverlay \
     NetworkStackOverlay \
