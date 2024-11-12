86d2f22382183d65b30bb6edd99350530ba65e69
Magicxavi <rolfpeterkesche@gmail.com>
2024-11-12 19:34:31 +0200

    pm: adapt vendor for PixelMagic

--- config/version.mk
+++ config/version.mk
@@ -1,5 +1,7 @@
+# Type of Build
 CUSTOM_BUILD_TYPE ?= UNOFFICIAL
 
+# Time Related
 CUSTOM_DATE_YEAR := $(shell date -u +%Y)
 CUSTOM_DATE_MONTH := $(shell date -u +%m)
 CUSTOM_DATE_DAY := $(shell date -u +%d)
@@ -8,7 +10,9 @@ CUSTOM_DATE_MINUTE := $(shell date -u +%M)
 CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONTH)-$(CUSTOM_DATE_DAY) $(CUSTOM_DATE_HOUR):$(CUSTOM_DATE_MINUTE) UTC' +%s)
 CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)$(CUSTOM_DATE_MINUTE)
 
+# Platform (Android Version)
 CUSTOM_PLATFORM_VERSION := 15.0
+PM_BUILD_VERSION = 15.0
 
 CUSTOM_VERSION := PixelMagic_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE)
 CUSTOM_VERSION_PROP := fifteen
@@ -27,6 +31,18 @@ ifeq ($(IS_OFFICIAL),true)
         net.pixelos.build_type=ci \
         net.pixelos.version=$(CUSTOM_VERSION_PROP)
 endif
+
+# Versioning props
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    org.pm.version=$(PM_BASE_VERSION) \
+    org.pm.build_date=$(CUSTOM_BUILD_DATE) \
+    org.pm.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
+    org.pm.build_type=$(CUSTOM_BUILD_TYPE) \
+    org.pm.codename=$(PM_BASE_VERSION) \
+    org.pm.build_version=$(PM_BUILD_VERSION) \
+    ro.pm.maintainer=$(PM_MAINTAINER) \
+    org.pm.device=$(TARGET_DEVICE_NAME) \
+    ro.pm.recovery.version=$(PM_RECOVERY_VERSION)
 
 # Signing
 ifneq (eng,$(TARGET_BUILD_VARIANT))
