72fa113b9afe9e1a36fa602e7e8f18369a96786f
magicxavi <rolfpeterkesche@gmail.com>
2025-03-03 10:53:33 +0200

    build: fix eat-ing the OTA zip
    Signed-off-by: Bharat Dev Burman <bharat.singh7924@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -64,7 +64,7 @@ alias bib=breakfast
 function eat()
 {
     if [ "$OUT" ] ; then
-        ZIPPATH=`ls -tr "$OUT"/PixelMagic-*.zip | tail -1`
+        ZIPPATH=`ls -tr "$OUT"/PixelMagic_*.zip | tail -1`
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
             return 1
